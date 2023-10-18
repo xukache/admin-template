fcfdb5c96d2bc3c7400bc7d286f5e05a359b8f8b
xukai <2416695482@qq.com>
2023-10-18 10:48:10 +0800

    测试提交代码，进行代码格式化

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,16 @@
 </template>
 
 <script setup lang="ts">
-const str = '我爱你祖国'
+const str = '我爱你祖国';
 </script>
 
 <style scoped lang="scss">
 .box {
-  width: 600px;
-  height: 400px;
-  background-color: black;
+  width:600px;
+  height:400px;
+  background-color:black;
   h1 {
-    color: white;
+    color:white;
   }
 }
 </style>

a7eb95b7f91e430a610b4cf307f9609a08c44ba5
xukai <2416695482@qq.com>
2023-10-18 10:58:55 +0800

    feat: 测试提交检测

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   <div class="box">
     <h1>我是App</h1>
     <p>{{ str }}</p>
+    <hr>
+    <h1>新增一个大标题</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-const str = '我爱你祖国';
+const str = '我爱你祖国'
 </script>
 
 <style scoped lang="scss">
 .box {
-  width:600px;
-  height:400px;
-  background-color:black;
+  width: 600px;
+  height: 400px;
+  background-color: black;
   h1 {
-    color:white;
+    color: white;
   }
 }
 </style>

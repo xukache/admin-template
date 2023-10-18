94cf9a47733e8cacf0f5400d3deb19352fe7d893
xukai <2416695482@qq.com>
2023-10-18 10:45:28 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <h1>我是App</h1>
+    <p>{{ str }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '我爱你祖国'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const fun = () => {
+  console.log('我爱你祖国')
+}
+
+fun()
